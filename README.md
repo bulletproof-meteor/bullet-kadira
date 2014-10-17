f8dfda3f4b1aa0dfd790ddf64ac257cf75e5a070
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-10-17 11:26:43 +0530

    initial version

---
+++ README.md
@@ -0,0 +1,5 @@
+# bullet-one: input validations
+
+This is a simple todo Meteor app which helps you to understand input validations.
+
+![Meteor Todo App Screenshot](https://i.cloudup.com/DCcBG58qYv.png)
---
+++ app.js
@@ -0,0 +1,49 @@
+Todos = new Meteor.Collection('todos');
+Todos.allow({
+  remove: function() { return true; },
+  update: function() { return true; }
+});
+
+Meteor.methods({
+  addTodo: function(title) {
+    var a =  Todos.insert({'title': title});
+    console.log('n8866666');
+  }
+});
+
+if(Meteor.isServer) {
+  Meteor.publish('todos', function() {
+    return Todos.find();
+  });
+}
+
+if(Meteor.isClient) {
+  Meteor.subscribe('todos');
+
+  Template.main.events({
+    'click #add-todo': function () {
+      var todoText = $('#input-todo').val();
+      if(todoText.trim() != ""){
+        Meteor.call('addTodo', todoText);
+        $('#input-todo').val('');
+      }
+    },
+
+    'click .delete-todo': function () {
+      Todos.remove(this._id);
+    },
+
+    'change .todo-done ': function(e){
+      var isDone = $(e.target).is(':checked');
+      Todos.update({_id: this._id}, {$set: {isDone: isDone}})
+    }
+  });
+
+  Template.main.checkedState = function() {
+    return this.isDone? "checked": "";
+  }
+
+  Template.main.todosList = function(){
+    return Todos.find();
+  }
+}
